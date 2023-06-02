1486d8f36579957c77d4aa9948bbae960dee6bca
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-02 23:36:17 +0600

    youtube embed component

---
+++ src/components/YouTubeEmbed/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './index.module.scss';
+const YouTubeEmbed = ({ embedUrl }: { embedUrl: string }) => {
+  console.log(embedUrl);
+  return (
+    <div className={styles.videoResponsive}>
+      <iframe
+        width="300"
+        height="225"
+        src={`${embedUrl}?autoplay=1`}
+        allowFullScreen
+        title="Trailer"
+        // frameBorder="0"
+        // controls={1}
+      />
+    </div>
+  );
+};
+
+export default YouTubeEmbed;
