88d17b1da10dbaec13463e7b8ea5e04e1dff5e48
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-02 23:30:30 +0600

    added slider for home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,37 @@
-import React from 'react';
 import useSWR from 'swr';
-import {
-  getMovieById,
-  getMoviesByFilters,
-  IFilters,
-} from '../../api/kinoboomApi';
-import Button from '../../components/ui/Button';
-import MovieCard from '../../components/MovieCard';
+import { getMovieById, IFilters } from '../../api/kinoboomApi';
+import MySlider from '../../components/MySlider';
+
+const filters: IFilters = {
+  ratingFrom: 0,
+  ratingTo: 10,
+  yearFrom: 2021,
+  yearTo: 2023,
+  order: 'NUM_VOTE',
+  type: 'ALL',
+};
 
 const Home = () => {
-  const filters: IFilters = {
-    genre: '',
-    ratings: '1-10',
-    years: '1960-2023',
-    sortByRelease: -1,
-  };
-  // const { data: movielist } = useSWR(
-  //   [filters, 1, 'человек паук'],
-  //   getMoviesByFilters
-  // );
   // console.log(movielist);
 
-  const { data: movie, isLoading } = useSWR('111543', getMovieById);
-  if (isLoading) return <h1>Loading...</h1>;
-  console.log(movie.docs[0]);
-  const props = movie.docs[0];
+  // const { data: movie, isLoading } = useSWR('111543', getMovieById);
+  // if (isLoading) return <h1>Loading...</h1>;
+  // console.log(movie?.docs[0]);
+  // const props = movie?.docs[0];
+
   return (
     <div>
-      {/* <img src={movie.poster.previewUrl} /> */}
-      <MovieCard
-        id={props.id}
-        url={props.poster.previewUrl}
-        title={props.name}
-        type={props.type}
-        rating={props.rating.imdb}
-        genres={props.genres}
+      <MySlider
+        title="Популярно сейчас"
+        filters={{ ...filters, yearFrom: 2022 }}
+      />
+      <MySlider
+        title="Популярные фильмы"
+        filters={{ ...filters, yearFrom: 2000 }}
+      />
+      <MySlider
+        title="Популярные сериалы"
+        filters={{ ...filters, yearFrom: 2000, type: 'TV_SERIES' }}
       />
     </div>
   );
