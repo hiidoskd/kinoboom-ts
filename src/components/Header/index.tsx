a0ce2e3850c5d6bfa01cbaa760d97f463e01ad7f
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-02 23:26:09 +0600

    added responsiveness and searchbar to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 import { Link } from 'react-router-dom';
 import useScrollListener from '../../hooks/useScrollListener';
+import Searchbar from '../Searchbar';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 
 const Header = () => {
   const [navClassList, setNavClassList] = useState<string[]>([]);
+  const [burgerOpen, setBurgerOpen] = useState(false);
   const scroll = useScrollListener();
 
   // update classList of nav on scroll
@@ -22,25 +25,35 @@ const Header = () => {
         navClassList.length ? styles[navClassList[0]] : ''
       }`}
     >
-      <div className={styles.logo}>KINOBOOM</div>
-      <div className={styles.itemContainer}>
-        <Link to="/" className={styles.navitem}>
-          Home
-        </Link>
-        <Link to="/" className={styles.navitem}>
-          Discover
-        </Link>
-        <Link to="/" className={styles.navitem}>
-          Movie Release
-        </Link>
-        <Link to="/" className={styles.navitem}>
-          Forum
-        </Link>
-        <Link to="/" className={styles.navitem}>
-          About
-        </Link>
+      <div className={styles.left}>
+        <div className={styles.logo}>KINOBOOM</div>
       </div>
-      <div>Icons</div>
+      <div className={styles.right}>
+        <div
+          className={`${styles.links} ${
+            burgerOpen ? styles.links__active : ''
+          }`}
+        >
+          <Link to="#">Главная</Link>
+          <Link to="#">Фильмы</Link>
+          <Link to="#">Сериалы</Link>
+        </div>
+        <Searchbar />
+        <div className={styles.avatar}>AVA</div>
+        <button
+          className={styles.burger}
+          onClick={() => {
+            if (!burgerOpen) {
+              document.body.style.overflow = 'hidden';
+            } else {
+              document.body.style.overflow = '';
+            }
+            setBurgerOpen((prev) => !prev);
+          }}
+        >
+          <HamburgerMenuIcon />
+        </button>
+      </div>
     </div>
   );
 };
