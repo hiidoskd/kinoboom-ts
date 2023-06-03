17054e270f98046fc073d698e809b73dc8c24c13
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-03 15:21:05 +0600

    refactored to use tables

--- src/components/MovieInfo/index.tsx
+++ src/components/MovieInfo/index.tsx
@@ -3,57 +3,61 @@ import styles from './index.module.scss';
 
 const MovieInfo = ({ movie }) => {
   return (
-    <div>
+    <div className={styles.info}>
       <h3>О фильме</h3>
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Год производства</div>
-        <div className={styles.rowItem}>{movie.year}</div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Страна</div>
-        <div className={styles.rowItem}>
-          {movie.countries.map((country, idx) => (
-            <Fragment key={country.country}>
-              {idx ? ', ' : ''}
-              {country.country}
-            </Fragment>
-          ))}
-        </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Жанр</div>
-        <div className={styles.rowItem}>
-          {movie.genres.map((genre, idx) => (
-            <Fragment key={genre.genre}>
-              {idx ? ', ' : ''}
-              {genre.genre}
-            </Fragment>
-          ))}
-        </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Слоган</div>
-        <div className={styles.rowItem}>{movie.slogan}</div>
-      </div>
-      {/* <div className={styles.row}>
+      <table>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Год производства</td>
+          <td className={styles.rowItem}>{movie.year}</td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Страна</td>
+          <td className={styles.rowItem}>
+            {movie.countries.map((country, idx) => (
+              <Fragment key={country.country}>
+                {idx ? ', ' : ''}
+                {country.country}
+              </Fragment>
+            ))}
+          </td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Жанр</td>
+          <td className={styles.rowItem}>
+            {movie.genres.map((genre, idx) => (
+              <Fragment key={genre.genre}>
+                {idx ? ', ' : ''}
+                {genre.genre}
+              </Fragment>
+            ))}
+          </td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Слоган</td>
+          <td className={styles.rowItem}>{movie.slogan}</td>
+        </tr>
+        {/* <div className={styles.row}>
         <div className={styles.rowItem}>Сборы</div>
         <div className={styles.rowItem}>
           {movie.fees.world.currency}
           {movie.fees.world.value}
         </div>
       </div> */}
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Дата премьеры</div>
-        <div className={styles.rowItem}>{movie.year}</div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Рейтинг</div>
-        <div className={styles.rowItem}>{movie.ratingAgeLimits}+</div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.rowItem}>Длительность</div>
-        <div className={styles.rowItem}>{movie.filmLength} минут</div>
-      </div>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Дата премьеры</td>
+          <td className={styles.rowItem}>{movie.year}</td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Рейтинг</td>
+          <td className={styles.rowItem}>
+            {movie.ratingAgeLimits ? movie.ratingAgeLimits : 0}+
+          </td>
+        </tr>
+        <tr className={styles.row}>
+          <td className={styles.rowItem}>Длительность</td>
+          <td className={styles.rowItem}>{movie.filmLength} минут</td>
+        </tr>
+      </table>
     </div>
   );
 };
