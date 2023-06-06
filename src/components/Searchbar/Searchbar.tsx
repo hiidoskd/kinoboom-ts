31f131ea754e4ce6be4c4f0fa4932f4e7f8fce96
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-07 02:14:45 +0600

    created suggestions when searching for a movie

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -3,38 +3,54 @@ import { useNavigate } from 'react-router-dom';
 import styles from './Searchbar.module.scss';
 import { MagnifyingGlassIcon, Cross2Icon } from '@radix-ui/react-icons';
 
+import SuggestionList from './SuggestionList/SuggestionList';
+import { useDebounce } from '../../hooks/useDebounce';
+
 const Searchbar = () => {
   const navgigate = useNavigate();
+  const [searchTerm, setValue] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
-  const [value, setValue] = useState('');
+  const handleChange = (keyword: string) => {
+    setValue(keyword);
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    navgigate(`/search/${value}`);
+    navgigate(`/search/${searchTerm}`);
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.searchbar}>
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          placeholder="Поиск..."
+    <div className={styles.mainContainer}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.searchbar}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => handleChange(e.target.value)}
+            placeholder="Поиск..."
+          />
+          <button
+            type="button"
+            className={`${styles.btnClose} ${
+              searchTerm && styles.btnCloseActive
+            }`}
+            onClick={() => setValue('')}
+          >
+            <Cross2Icon width="18" height="18" />
+          </button>
+          <button type="submit" className={styles.btnSearch}>
+            <MagnifyingGlassIcon width="21" height="21" />
+          </button>
+        </div>
+      </form>
+      {debouncedSearchTerm && (
+        <SuggestionList
+          key={debouncedSearchTerm}
+          keyword={debouncedSearchTerm}
         />
-        <button
-          type="button"
-          className={`${styles.btnClose} ${value && styles.btnCloseActive}`}
-          onClick={() => setValue('')}
-        >
-          <Cross2Icon width="18" height="18" />
-        </button>
-      </div>
-
-      <button type="submit" className={styles.btnSearch}>
-        <MagnifyingGlassIcon width="21" height="21" />
-      </button>
-    </form>
+      )}
+    </div>
   );
 };
 
