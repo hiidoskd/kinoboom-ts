a525f547b4d3e7d6ee22b01bbd1c9de4a5153bfe
Yeldar Issa <yeldar.issa@gmail.com>
2023-06-07 02:14:28 +0600

    bugfix

--- src/components/ui/Subtitle/Subtitle.tsx
+++ src/components/ui/Subtitle/Subtitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Subtitle.module.scss';
 
 type SubtitleProps = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
 };
 
